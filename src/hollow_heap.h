04e23fe59bb9b9b93f83ad52143e1da96f31063f
Arseniy Kouzmenkov <arseniy.kouzmenkov@gmail.com>
2020-04-05 21:12:52 -0600

    implemented everything but decrease key. Might be an issue.

--- src/hollow_heap.h
+++ src/hollow_heap.h
@@ -1,3 +1,6 @@
+#include<vector>
+#include<cstddef>
+#include<iostream>
 
 template< class T, class K >
 struct hollow_heap_item;
@@ -32,6 +35,7 @@ class hollow_heap {
         void delete_min();
         int get_size();
         bool is_empty();
+        void dump_state();
         
     private:
         hollow_heap_node< T, K >* heap;
@@ -48,7 +52,32 @@ class hollow_heap {
         void do_ranked_links( hollow_heap_node< T, K >* u );
         void do_unranked_links( );
         int size;
+        std::vector< hollow_heap_node< T, K >* > A;
+        
 };
+
+template < class T, class K >
+void hollow_heap< T, K >::dump_state() {
+    hollow_heap_node< T, K >* u = heap;
+    hollow_heap_node< T, K >* v;
+    std::cout << "Starting from the root" << std::endl;
+    while( u ) {
+        std::cout << "   Key: " << u->key << " Value: " << u->item->item << std::endl;
+        std::cout << "   Iterating through children" << std::endl;
+        v = heap->child;
+        while( v ) {
+            std::cout << "       Key: " << v->key << " Value: " << v->item->item << std::endl;
+            if ( !v->next ) {
+                v = v->child;
+                continue;
+            }
+            v = v->next;
+        }
+
+        u = u->next;
+    } 
+}
+
 
 template < class T, class K >
 hollow_heap< T, K >::hollow_heap() {
@@ -59,6 +88,7 @@ hollow_heap< T, K >::hollow_heap() {
 
 template < class T, class K >
 hollow_heap< T, K >::~hollow_heap() {
+    while( heap ) delete_min();    
     // Figure out what needs to be destroyed
 }
 
@@ -69,25 +99,37 @@ hollow_heap< T, K > hollow_heap< T, K >::make_heap() {
 
 template < class T, class K >
 void hollow_heap< T, K >::insert( T e, K k ) {
-    // Implement the insertion from the pseudo-code.
+    meld( make_node( e, k ), heap );
+    size++;
 }
 
 template < class T, class K >
 void hollow_heap< T, K >::meld( 
         hollow_heap_node< T, K >* h,
         hollow_heap_node< T, K >* g ) {
-    // Implement the melding from the pseudo-code.
+    if ( h == nullptr ) { 
+        heap = g;
+        return;
+    }
+    if ( g == nullptr ) { 
+        heap = h;
+        return;
+    }
+    heap = link( h, g );
 }
 
 template < class T, class K >
 T hollow_heap< T, K >::find_min() {
-    if ( heap == nullptr ) return nullptr;
+    if ( heap == nullptr ) { 
+        //std::cout << "Shouldn't be here -------------------------" << std::endl;
+        return NULL;
+    }
     return heap->item->item;
 }
 
 template < class T, class K >
 void hollow_heap< T, K >::delete_min() {
-    // Implement the delete_min from the pseudo-code.
+    delete_item( heap->item );
 }
 
 template < class T, class K >
@@ -104,14 +146,27 @@ template < class T, class K >
 hollow_heap_node< T, K >* hollow_heap< T, K >::link( 
         hollow_heap_node< T, K >* v,
         hollow_heap_node< T, K >* w ) {
-    // Implement the link from the pseudo-code. Temporarily, returns NULL;
-    return nullptr;
+    if ( v->key >= w->key ) {
+        add_child( v, w );
+        return w;
+    } else {
+        add_child( w, v );
+        return v;
+    }
 }
 
 template < class T, class K >
 hollow_heap_node< T, K >* hollow_heap< T, K >::make_node( T e, K k ){
-    // Implement the make_node from the pseudo code. Temporarily returns NULL;
-    return nullptr;
+    hollow_heap_node< T, K >* u = new hollow_heap_node< T, K >();
+    u->item = new hollow_heap_item< T, K >();
+    u->item->item = e;
+    u->child = nullptr;
+    u->next = nullptr;
+    u->ep = nullptr;
+    u->key = k;
+    u->item->node = u;
+    u->rank = 0;
+    return u;
 }
 
 template < class T, class K >
@@ -123,20 +178,68 @@ template < class T, class K >
 void hollow_heap< T, K >::add_child( 
         hollow_heap_node< T, K >* v,
         hollow_heap_node< T, K >* w ) {
-    // Implement add_child from the pseudo code.
+    v->next = w->child;
+    w->child = v;
 }
 
 template < class T, class K >
 void hollow_heap< T, K >::delete_item( hollow_heap_item< T, K >* item ){
-    // Implement delete_item from the pseudo code.
+    //delete item;
+    item->node->item = nullptr;
+    delete item;
+    size--;
+    if ( heap->item != nullptr ) return;
+    max_rank = 0;
+    while( heap != nullptr ) {
+        hollow_heap_node< T, K >* w = heap->child;
+        hollow_heap_node< T, K >* v = heap;
+        heap = heap->next;
+        while( w != nullptr ) {
+            //std::cout << "Deleting" << std::endl;
+            hollow_heap_node< T, K >* u = w;
+            w = w->next;
+            if ( u->item == nullptr ) {
+                if ( u->ep == nullptr ) {
+                    u->next = heap;
+                    heap = u;
+                } else {
+                    if ( u->ep == v ) w = nullptr;
+                    else u->next = nullptr;
+                    u->ep = nullptr;
+                }
+            } else {
+                do_ranked_links( u );
+            }
+        }
+        delete v;
+    }
+    do_unranked_links();
 }
 
 template < class T, class K >
 void hollow_heap< T, K >::do_ranked_links( hollow_heap_node< T, K >* u ) {
-    // Implement do_ranked_links from the pseudo code.
+    while( u->rank >= A.size() ){
+        A.push_back( nullptr );
+    }
+    while ( A[ u->rank ] != nullptr ) {
+        u = link( u, A[ u->rank ] );
+        //A[ u->rank ] = nullptr;
+        A[ u->rank ] = nullptr;
+        u->rank++;
+    }
+    A[ u->rank ] = u;
+
+    if ( u->rank > max_rank ) max_rank = u->rank;
 }
 
 template < class T, class K >
 void hollow_heap< T, K >::do_unranked_links( ) {
-    // Implement do_unranked_links from the pseudo code.
+    for ( int i = 0; i <= max_rank; i++ ) {
+        if ( A[i] != nullptr ) {
+            //std::cout << "Going deep" << std::endl;
+            if ( heap == nullptr ) heap = A[i];
+            else heap = link( heap, A[i] );
+            A[i] = nullptr;
+        }
+    }
 }
--- src/quick_test.cpp
+++ src/quick_test.cpp
@@ -3,8 +3,24 @@
 using namespace std;
 
 int main() {
-    hollow_heap<int, int> h();
+    hollow_heap<int, int> h = hollow_heap<int, int>();
     cout << "Initialization test passed" << endl;
+
+    h.insert( 3, 3 );
+    cout << h.find_min() << " " << h.get_size() << endl;
+
+    h.insert( 2, 2 );
+    cout << h.find_min() << " " << h.get_size() << endl;
+    h.insert( 1, 1 );
+    cout << h.find_min() << " " << h.get_size() << endl;
+    h.dump_state();
+
+    h.delete_min();
+    //if ( h.find_min() == NULL ) cout << "UHHH" << endl;
+    cout << h.find_min() << " " << h.get_size() << endl;
+    //h.dump_state();
+    cout << h.find_min() << " " << h.get_size() << endl;
+    cout << "Survived that." << endl;
 
 
     return 0;
